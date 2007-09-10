fb277fdce21f1c7619f60c1879a6ce7af0bc4304
AudinChan <AudinChan@ed583cf7-8739-0410-bdc0-d123cdd6f987>
2007-09-10 15:55:49 +0000

    增加用户设置修改

--- trunk/rent/WebContent/common/userinfo.jsp
+++ trunk/rent/WebContent/common/userinfo.jsp
@@ -3,6 +3,7 @@
 <%@ include file="/common/taglibs.jsp" %>
 <c:if test="${not empty sessionScope[userKey]}">
 Hello, ${sessionScope[userKey].displayName} [<a href="${ctx }/j_acegi_logout">退出</a>]
+<a href='${ctx }/user/Profile.a'>个人设置</a>
 </c:if>
 <c:if test="${empty sessionScope[userKey]}">
 [<a href="${ctx }/reg/Register.a">注册</a> | 
--- trunk/rent/src/com/yodoo/rent/webapp/action/user/Profile.java
+++ trunk/rent/src/com/yodoo/rent/webapp/action/user/Profile.java
@@ -3,9 +3,19 @@
  */
 package com.yodoo.rent.webapp.action.user;
 
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
+import org.nestframework.addons.spring.Spring;
 import org.nestframework.annotation.DefaultAction;
 
-import com.yodoo.rent.commons.NotImplementedException;
+import com.yodoo.rent.commons.Constant;
+import com.yodoo.rent.model.User;
+import com.yodoo.rent.model.UserProfile;
+import com.yodoo.rent.service.IUserManager;
+import com.yodoo.rent.service.IUserProfileManager;
+import com.yodoo.rent.webapp.action.BaseAction;
 
 /**
  * 管理用户Profile.
@@ -13,29 +23,58 @@ import com.yodoo.rent.commons.NotImplementedException;
  * @author audin
  *
  */
-public class Profile {
+public class Profile extends BaseAction {
 	/**
 	 * 显示我的Profile.
 	 * @return
 	 */
 	@DefaultAction
-	public Object show() {
-		throw new NotImplementedException();
+	public Object show(HttpSession s) {
+		User user = getLoginUser(s);
+		Set<UserProfile> ups = user.getUserProfiles();
+		if (! ups.isEmpty()) {
+			//profile = ups.get(0);
+		}
+		return "/user/profile_show.jsp";
 	}
 	
 	/**
 	 * 编辑我的Profile.
 	 * @return
 	 */
-	public Object edit() {
-		throw new NotImplementedException();
+	public Object edit(HttpSession s) {
+		show(s);
+		return "/user/profile_edit.jsp";
 	}
 	
 	/**
 	 * 保存我的Profile.
 	 * @return
 	 */
-	public Object save() {
-		throw new NotImplementedException();
+	public Object save(HttpSession s) {
+		User user = getLoginUser(s);
+		profile.setUser(user);
+		userProfileManager.save(profile);
+		s.setAttribute(Constant.USER_KEY, userManager.get(user.getUsername()));
+		return "/user/profile_show.jsp";
+	}
+	
+	@Spring
+	private IUserManager userManager;
+	
+	@Spring
+	private IUserProfileManager userProfileManager;
+	
+	/**
+	 * 用户设置.
+	 */
+	private UserProfile profile = new UserProfile();
+
+	public UserProfile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(UserProfile profile) {
+		this.profile = profile;
 	}
 }
