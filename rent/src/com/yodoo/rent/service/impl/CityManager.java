3cf161c23666692930efec67113d32173999b918
AudinChan <AudinChan@ed583cf7-8739-0410-bdc0-d123cdd6f987>
2007-09-11 14:46:29 +0000

    添加城市和区域表以及对应的配置文件等

---
+++ rent/src/com/yodoo/rent/service/impl/CityManager.java
@@ -0,0 +1,10 @@
+package com.yodoo.rent.service.impl;
+// Generated 2007-9-11 22:39:33 by Hibernate Tools 3.2.0.b9 with mintgen
+
+
+import com.yodoo.rent.model.City;
+import com.yodoo.rent.service.ICityManager;
+
+public class CityManager extends BaseManager<City, String> implements ICityManager {
+
+}
